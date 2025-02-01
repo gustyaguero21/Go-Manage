e11836937b56d47d88247e8f27a810d622bd7301
Gustavo Aguero Ferrari <gustyaguero21@gmail.com>
2025-02-01 20:45:34 -0300

    Commit: adding database

--- internal/router/urlmapping.go
+++ internal/router/urlmapping.go
@@ -1,6 +1,7 @@
 package router
 
 import (
+	"go-manage/internal/data"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -8,6 +9,7 @@ import (
 
 func Urlmapping(r *gin.Engine) {
 	api := r.Group("/api/go-manage")
+	data.InitDatabase()
 
 	api.GET("/ping", func(ctx *gin.Context) {
 		ctx.JSON(http.StatusOK, "pong")
